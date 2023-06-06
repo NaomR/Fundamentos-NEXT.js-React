84af17caf2d08b09392806bf04e2d664e7680354
Naom Ricardo <78932401+NaomR@users.noreply.github.com>
2023-06-06 17:40:07 -0300

    segundo commit - integração css

---
+++ src/pages/css/integracao2.jsx
@@ -0,0 +1,10 @@
+import styles from './integracao2.module.css'
+
+export default function integracao2 () {
+    return (
+        <div id={styles.integra2}>
+            <div className={styles.roxo}>Teste 01 - div em roxo</div>
+            <div className={styles.azul}>teste 02 - div em azul</div>  
+        </div>
+    )
+}
